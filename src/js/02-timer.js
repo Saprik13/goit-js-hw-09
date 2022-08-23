3ab8f3ece524c3f2af6394095328eb054833a311
Petro Sapronov <saprik13@gmail.com>
2022-08-24 00:04:09 +0300

    Update 02-timer.js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,7 +1,7 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
 import Notiflix from 'notiflix';
-import { Notify } from 'notiflix/build/notiflix-notify-aio';
+// import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 let selectedTime = null;
 const options = {
